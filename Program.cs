c1581a3219ca0bfdae38f6594171a8277353f2de
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2025-12-01 09:40:39 -0400

    Sixth Commit

--- Program.cs
+++ Program.cs
@@ -12,20 +12,29 @@ namespace GeorgiaDavid_TriviaGame
         static int numberOfQuestions = 10;
         static int currentQuestion;
 
-        static string playerName = ("Player");
+        static string playerName;
 
         static int correctAnswers = 0;
         static void Main(string[] args)
         {
-            
+            PlayerName();
         }
 
         static void DisplayHUD()
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Player");
+            Console.WriteLine(playerName);
             Console.WriteLine("Question: " + questions[currentQuestion]);
             Console.WriteLine("Score: " + correctAnswers);
+        }
+
+        static void PlayerName()
+        {
+            Console.WriteLine("Enter your Name");
+            playerName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Welcome, " + playerName);
+            Console.ReadKey();
         }
     }
 }
