7f69a50a9b279658262ce82132d3e955cb607963
GeorgiaDavid2006 <W0512995@campus.nscc.ca>
2025-12-09 14:34:06 -0400

    Twenty-fourth Commit

--- Program.cs
+++ Program.cs
@@ -34,23 +34,102 @@ namespace GeorgiaDavid_TriviaGame
             "Health is equal to zero",
             "Health is not less than or equal to zero",
             "Health is less than or equal to zero",
-            "Health is less than zero"
+            "Health is less than zero",
+            "Answer9",
+            "Answer10",
+            "Answer11",
+            "Answer12",
+            "Answer13",
+            "Answer14",
+            "Answer15",
+            "Answer16",
+            "Answer17",
+            "Answer18",
+            "Answer19",
+            "Answer20",
+            "Answer21",
+            "Answer22",
+            "Answer23",
+            "Answer24",
+            "Answer25",
+            "Answer26",
+            "Answer27",
+            "Answer28",
+            "Answer29",
+            "Answer30",
+            "Answer31",
+            "Answer32",
+            "Answer33",
+            "Answer34",
+            "Answer35",
+            "Answer36",
+            "Answer37",
+            "Answer38",
+            "Answer39",
+            "Answer40",
         };
 
         static int answerNumber = 0;
 
         static string playerName;
 
-        static int[] correctAnswers = {3, 2};
+        static int[] correctAnswers = {3, 2, 1, 1, 1, 1, 1, 1, 1, 1};
 
         static bool checkingForParse = true;
+
+        static bool isPlaying = true;
 
         static int points = 0;
 
         static void Main(string[] args)
         {
-            PlayerName();
-            AskQuestion();
+            while(isPlaying)
+            {
+                PlayerName();
+                AskQuestion();
+                DisplayScore();
+            }
+        }
+
+        static void DisplayScore()
+        {
+            bool playerEndPrompt = true; 
+
+            while(playerEndPrompt == true)
+            {
+                Console.WriteLine($"You got {points / questions.Length}% of the questions right"); 
+                if(points/questions.Length == 100)
+                {
+                    Console.WriteLine("Congratulations! You got everything right!");
+                }
+                else if(points/questions.Length == 0)
+                {
+                    Console.WriteLine("Maybe you need to brush up on your programming knowledge");
+                }
+                else
+                {
+                    Console.WriteLine("Well done");
+                }
+                    Console.WriteLine("Would you like to play again? [y/n]");
+                string playerContinue = Console.ReadLine();
+                if (playerContinue == "Y" || playerContinue == "y")
+                {
+                    playerEndPrompt = false;
+                    points = 0;
+                    answerNumber = 0;
+                }
+                else if (playerContinue == "N" || playerContinue == "n")
+                {
+                    isPlaying = false;
+                    playerEndPrompt = false;
+                    Console.WriteLine("Thank you for playing");
+                    Console.ReadKey();
+                }
+                else
+                {
+
+                }
+            }  
         }
 
         static void DisplayHUD(int currentQuestion)
@@ -81,19 +160,16 @@ namespace GeorgiaDavid_TriviaGame
 
         static void AskQuestion()
         {
-            checkingForParse = true;
-            
-
             for (int question = 0; question < questions.Length; question++)
             {
                 DisplayHUD(question);
-                for (int answer = 0; answer < 4; answer++)
+                for (int answer = 0; answer < answers.Length/questions.Length; answer++)
                 {
                     Console.WriteLine($"\n{answer + 1} {answers[answerNumber]}");
                     answerNumber++;
                 }
-                
 
+                checkingForParse = true;
                 while (checkingForParse == true)
                 {
                     string playerAnswer = Console.ReadLine();
